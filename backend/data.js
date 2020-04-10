abfe0a77abe8ce5398a8a0a8b76057fb05ff9782
Firoz Ahmed Bhuyan <firoz@Firozs-MBP.hub>
2020-04-11 02:47:58 +0800

    first commit

---
+++ backend/data.js
@@ -0,0 +1,37 @@
+export default {
+    products: [{
+            _id: '1',
+            name: 'Slim-shirt',
+            category: 'shirts',
+            image: '/images/d1.jpeg',
+            price: 50,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 10
+        },
+        {
+            _id: '2',
+            name: 'slim-shirt2',
+            category: 'shirts',
+            image: '/images/d1.jpeg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6
+        },
+        {
+            _id: '3',
+            name: 'paint1',
+            category: 'paints',
+            image: '/images/d1.jpeg',
+            price: 70,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 5
+        }
+
+    ]
+}
